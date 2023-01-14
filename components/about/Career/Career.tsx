3adc1d78edffd60f9df6f892d9fb17c97bce3dc1
jsm8109jsm <jsm8109jsm@gmail.com>
2023-01-14 21:27:34 +0900

    FIX :: Career 데이터 설정

--- components/about/Career/Career.tsx
+++ components/about/Career/Career.tsx
@@ -1,12 +1,22 @@
 import React from "react";
 import * as S from "./Career.style";
 
-function Career({ title }: { title: string }) {
+function Career({
+  title,
+  data,
+  isCertificate,
+}: {
+  title: string;
+  data: string[];
+  isCertificate?: true;
+}) {
   return (
     <S.Career>
       <S.Title>{title}</S.Title>
       <S.Contents>
-        <S.Content isCertificate>정보처리산업기사</S.Content>
+        {data.map((item, index) => {
+          return <S.Content isCertificate={isCertificate} key={index}>{item}</S.Content>;
+        })}
       </S.Contents>
     </S.Career>
   );
