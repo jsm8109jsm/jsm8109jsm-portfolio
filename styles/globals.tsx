ba6db8626ec3c5e26b52595a4877193eee349bc5
jsm8109jsm <jsm8109jsm@gmail.com>
2023-02-07 21:02:13 +0900

    FEAT :: custom scroll bar

--- styles/globals.tsx
+++ styles/globals.tsx
@@ -13,6 +13,24 @@ const GlobalStyle = createGlobalStyle`
     cursor: pointer;
   }
 
+  body{
+    &::-webkit-scrollbar {
+      width: 8px; /* 스크롤바의 너비 */
+    }
+
+    &::-webkit-scrollbar-thumb {
+      height: 30%; /* 스크롤바의 길이 */
+      background: ${({ theme }: any) => theme.color.teal}; /* 스크롤바의 색상 */
+
+      border-radius: 10px;
+    }
+
+    &::-webkit-scrollbar-track {
+      background: ${({ theme }) =>
+        theme.color.lightBeige}; /* 스크롤바의 색상 */
+    }
+  }
+
 `;
 
 export default GlobalStyle;
